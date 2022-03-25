923f576025f6b03d7ec5e9223832a3e4c6c2e06d
Abdullah Çelik <abdullahcelkk@gmail.com>
2022-03-25 19:40:25 +0300

    add basket functions

--- app.js
+++ app.js
@@ -34,3 +34,66 @@ const prevSlide = () => {
  let lastSlide = sliderImages[sliderImages.length - 1];
  sliderImages[0].before(lastSlide);
 }
+
+const Kitaplar = [
+ {
+  id: 1,
+  image: "assets/images/karatay-sozu.jpg",
+  qty: 2,
+  unitPrice: 46.99
+ },
+ {
+  id: 2,
+  image: "assets/images/kusur-imzadır.jpg",
+  qty: 1,
+  unitPrice: 34.99
+ },
+ {
+  id: 3,
+  image: "assets/images/suc-ve-ceza.jpg",
+  qty: 3,
+  unitPrice: 23.99
+ },
+];
+
+const basketTable = document.querySelector("#basket-table");
+console.log(basketTable);
+
+const createBasketItems = (kitap) => {
+ let satirHtml = `<tr class="tr-centered">
+<td>
+  <a href="./detail.html">
+    <img
+      src="${kitap.image}"
+      class="book-img"
+      alt="..."
+  /></a>
+</td>
+<td class="unit-price">
+  ${kitap.qty}
+  <div class="d-flex flex-column ms-3">
+    <i class="fa-solid fa-angle-up increase-decrease-btn" onclick="increaseQtyToBook(${kitap.id})"></i>
+    <i class="fa-solid fa-angle-down increase-decrease-btn"></i>
+  </div>
+</td>
+<td>${kitap.unitPrice} TL</td>
+<td>${(kitap.unitPrice * kitap.qty).toFixed(2)} TL</td>
+</tr>`;
+ basketTable.children[1].insertAdjacentHTML("beforeend", satirHtml);
+}
+
+const listBasketItems = () => {
+ Kitaplar.forEach(book => {
+  createBasketItems(book);
+ });
+}
+
+const increaseQtyToBook = (selectedId) => {
+ index = Kitaplar.findIndex(item => item.id == selectedId);
+ Kitaplar[index].qty += 1;
+ basketTable.children[1].innerHTML = "";
+ listBasketItems();
+}
+
+
+listBasketItems();
